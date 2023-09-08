968552abf1452998c3e894421d9660ffc1646e04
Philippos123 <>
2023-09-08 12:29:53 +0000

    Add JavaScript code for changing language of the website

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let Engbutton = document.getElementById("eng");
+let Swebutton = document.getElementById("swe");
+
+
+Engbutton.addEventListener("click", function(){
+window.location.href="index.html";
+});
+
+Swebutton.addEventListener("click", function(){
+    window.location.href="index_swedish.html";
+    
+    });
+
+    
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
