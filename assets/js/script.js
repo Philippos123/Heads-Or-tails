1c7a13134a91177114db96871188ff15ce406c7a
Philippos123 <>
2023-09-08 13:32:49 +0000

    Add JavaScript code for coinflip game and right or wrong counter for the english html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* Change Language through image*/
 let Engbutton = document.getElementById("eng");
 let Swebutton = document.getElementById("swe");
 
@@ -11,15 +12,34 @@ Swebutton.addEventListener("click", function(){
     
     });
 
-    
+    /*Right or Wrong tracker*/
+    let rightCounter =0;
+    let wrongCounter =0;
+ /*Coinflip Game*/
+   function coinflip() {
+    let randomNumber = Math.random ();
+    return randomNumber <0.5 ? "Heads": "Tails";
+   };   
+   
+   /* ButtonClick interaction*/
+   function buttonClick(userChoice){
+    let resultElement = document.getElementById("result");
+    let coinOutcome = coinflip();
+    let rightScoreElement = document.getElementById("right");
+    let wrongScoreElement = document.getElementById("wrong");
+    resultElement.textContent= `Coin has landed on ${coinOutcome}`
 
 
+    /*If & Else actions depening on the users pick */
+    if (userChoice.toLowerCase() === coinOutcome.toLowerCase()) {
+        resultElement.textContent += " Congratulations, you win!";
+        rightCounter++;
+      } else {
+        resultElement.textContent += " Sorry, you lose!";
+        wrongCounter++;
+      }
 
-
-  
-  
-  
-  
-  
-  
-  
+    rightScoreElement.textContent = `Right ${rightCounter}`;
+    wrongScoreElement.textContent = `Wrong ${wrongCounter}`;
+   
+    }
